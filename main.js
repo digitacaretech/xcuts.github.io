badbb71b9b0b90ee854d78028e86880578ac89d1
digitacaretech <92943581+digitacaretech@users.noreply.github.com>
2023-10-10 13:49:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+var swiper = new Swiper(".LightBoxSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: true,
+  centerSlide:'true',
+  fade:'true',
+  gragCursor: 'true',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:1
+    },
+    576:{
+      slidesPerView:2
+    },
+    768:{
+      slidesPerView:3
+    },
+    992:{
+      slidesPerView:4
+    }
+  }
+});
